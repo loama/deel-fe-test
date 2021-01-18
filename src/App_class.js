13f0c8cf612c8c0cddb4eaf5ce9c54c44e61cc98
Jaime López <loama18@gmail.com>
2021-01-18 12:47:03 -0600

    empty messages

--- src/App_class.js
+++ src/App_class.js
@@ -14,21 +14,30 @@ class App extends React.Component {
     this.setState({ value: value });
     const self = this;
 
-    axios.get("/api/countries.json").then(function (response) {
-      const data = response.data;
-      const options = data.filter((country) =>
-        country.toLowerCase().includes(value.toLowerCase())
-      );
-      console.log(options);
-      self.setState({
-        options: options,
-      });
+    const response = await axios.get("/api/countries.json");
+
+    const data = response.data;
+    const options = data.filter((country) =>
+      country.toLowerCase().includes(value.toLowerCase())
+    );
+    console.log(options);
+    self.setState({
+      options: options,
     });
   }
 
   render() {
     const options = this.state.options;
     const listItems = options.map((option) => <li>{option}</li>);
+
+    let emptyMessage = "";
+    if (options.length === 0) {
+      if (this.state.value.length > 0) {
+        emptyMessage = "No results matching your input";
+      } else {
+        emptyMessage = "Type something to search";
+      }
+    }
 
     return (
       <div>
@@ -40,6 +49,7 @@ class App extends React.Component {
         <div>{}</div>
         {this.state.value}
         {listItems}
+        <div className="emptyMessage">{emptyMessage}</div>
       </div>
     );
   }
